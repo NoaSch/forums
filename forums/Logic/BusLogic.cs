f71d4f53228417e2e0a1bdf53eb5697e8e5cac56
NoaSch <noasch4@gmail.com>
2017-06-15 14:45:38 +0300

    Add project files.

---
+++ forums/Logic/BusLogic.cs
@@ -0,0 +1,32 @@
+﻿using forums.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forums.Logic
+{
+
+        //class for the business logic layer
+        public class BusLogic
+        {
+
+            myData data;
+
+            public BusLogic()
+            {
+                data = new myData();
+            }
+
+
+        public bool testFunc()
+        {
+            return data.checkIfUserExist("noaschsFDF4@gmail.com");
+                
+        }
+
+
+
+}
+}
---
+++ forums/View/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using forums.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace forums
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BusLogic model;
+        public MainWindow()
+        {
+            InitializeComponent();
+            model = new BusLogic();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(model.testFunc())
+                System.Windows.MessageBox.Show("true");
+            else
+                System.Windows.MessageBox.Show("false");
+        }
+    }
+}
