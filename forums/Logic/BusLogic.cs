f12de0f28aebb3de81e66baddbbb62f330e805e8
elinorsh <elinorsh@post.bgu.ac.il>
2017-06-22 11:15:01 +0300

    get and set

--- forums/Logic/BusLogic.cs
+++ forums/Logic/BusLogic.cs
@@ -8,28 +8,31 @@ using System.Threading.Tasks;
 namespace forums.Logic
 {
 
-        //class for the business logic layer
-        public class BusLogic
-        {
-           System system;
-           protected myData db;
+    //class for the business logic layer
+    public class BusLogic
+    {
+        System system;
+        protected myData db;
 
-            public BusLogic(bool init)
-            {
+        public BusLogic(bool init)
+        {
             db = new myData();
-            if(init )
+            if (init)
                 system = new System();
-            
+
         }
 
 
-        public Dictionary<string,Tuple<string,string>> testFunc()
+        public Dictionary<string, Tuple<string, string>> testFunc()
         {
-           // return data.checkIfUserExist("noaschsFDF4@gmail.com");
-                return db.getMembersOfForum("1");                
+            // return data.checkIfUserExist("noaschsFDF4@gmail.com");
+            return db.getMembersOfForum("1");
         }
 
 
-        public System ForumsSys { get=> system; }
+        public System ForumsSys
+        {
+            get { return system; }
+        }
     }
 }
--- forums/Logic/System.cs
+++ forums/Logic/System.cs
@@ -3,23 +3,27 @@ using System.Collections.Generic;
 
 namespace forums.Logic
 {
-    public class System:BusLogic
+    public class System : BusLogic
     {
-        
+
         Dictionary<string, Forum> forums;
-        
-        public System():base(false)
+
+        public System() : base(false)
         {
             //the forums by subjects
             forums = new Dictionary<string, Forum>();
             List<string> forumsName = db.getForums();
-            foreach (string subject  in forumsName)
+            foreach (string subject in forumsName)
             {
                 forums.Add(subject, new Forum(subject));
             }
         }
 
 
-        public Dictionary<string, Forum> Forums { get => forums; set => forums = value; }
+        public Dictionary<string, Forum> Forums
+        {
+            get { return forums; }
+            set { forums = value; }
+        }
     }
 }
